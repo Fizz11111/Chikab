54e00eef0d845509d54c02df2dfeee33688accb5
Fizz11111 <99799307+Fizz11111@users.noreply.github.com>
2023-03-16 19:09:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6288296172396','6288296172396','6288296172396'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6288296172396'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '628296172396' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = '𝐅𝐢𝐳𝐳𝐱' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = '𝐇𝐢𝐬𝐨𝐤𝐚𝐁𝐨𝐭' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '𝐅𝐢𝐳𝐳𝐱' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
-global.region = 'indonesia'
+global.youtube = 'https://youtube.com/@imfizzx8729'
+global.ig = 'https://instagram.com/fizzx24?igshid=YmMyMTA2M2Y='
+global.mygc = 'https://chat.whatsapp.com/G6Y4d1TM9D4KOPXecMFdpJ'
+global.myweb = 'https://lynk.id/fizzz'
+global.email = 'fizzfeedback@gmail.com'
+global.region = 'Japan'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© 𝐇𝐢𝐬𝐨𝐤𝐚𝐁𝐨𝐭' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = '𝐅𝐢𝐳𝐳𝐱' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 20,
 }
 
 //—————「 Set Image 」—————//
